7f4f6e93702909cfb2c1e99958c87583b659e0c7
Vento <github@brenneisen.me>
2022-11-09 12:52:21 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+	$colorPerSize = [];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>T-Shirt Bestellen</title>
+</head>
+<body>
+	<form >
+		<input />
+		<input />
+		<input />
+		<input />
+		<input />
+		
+		<input type="submit" />
+	</form>
+</body>
