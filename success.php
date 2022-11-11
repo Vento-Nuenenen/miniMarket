d01fa2cb2b4197cda0ea300a2add17c2698ae6f6
Vento <github@brenneisen.me>
2022-11-11 13:31:27 +0100

    Update success.php

--- success.php
+++ success.php
@@ -1,2 +1,33 @@
 <?php
   
+?>
+
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>T-Shirt Bestellen</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+    <div id="app">
+        <main class="py-4">
+            <div class="container-fluid text-center">
+                <h1>Deine Bestellung: </h1>
+            </div>
+
+            <br />
+
+            <div class="container-fluid">
+              
+            </div>
+        </main>
+
+        <script src="https://kit.fontawesome.com/b7304ac133.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    </div>
+</body>
