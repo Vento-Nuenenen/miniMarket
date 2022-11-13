85856298bea1ca28c9c8901f02b30c8dcb2eb0ea
Vento <github@brenneisen.me>
2022-11-13 21:44:17 +0100

    Update order.php

--- order.php
+++ order.php
@@ -1,2 +1,23 @@
 <?php
-  
+    include 'dbconnect.php';
+
+    $name = $_POST['name'];
+    $contact = $_POST['contact'];
+    $shirt_size = $_POST['shirt_size'];
+    $shirt_color = $_POST['shirt_color'];
+    $print_color = $_POST['print_color'];
+    $print_placing = $_POST['print_placing'];
+    $byos = $_POST['byos'] ? 1 : 0;
+    $count = $_POST['count'];
+
+    $query = "INSERT INTO `minimarket`
+        (`name`, `contact`, `shirt_size`, `shirt_color`, 
+         `print_color`, `print_placing`, `byos`, `count`) 
+        VALUES ('$name','$contact','$shirt_size','$shirt_color',
+                '$print_color','$print_placing','$byos','$count')";
+
+    print_r($query);
+    $bestellung = mysqli_query($db, $query);
+    print_r($bestellung);
+
+
