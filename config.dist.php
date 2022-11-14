915fc6f65f876fb11db20d30455f744af1991fe1
Caspar Brenneisen <github@brenneisen.me>
2022-11-14 18:11:20 +0100

    Update config.dist.php

--- config.dist.php
+++ config.dist.php
@@ -1,7 +1,12 @@
 <?php
-  return [
-    'dbhost' => "localhost",
-    'dbuser' => "root",
-    'dbpass' => "1234",
-    'dbname' => "minimarket",
-  ];
+
+    return [
+        'dbhost' => "localhost",
+        'dbuser' => "root",
+        'dbpass' => "1234",
+        'dbname' => "minimarket",
+        'totalPriceImport' => 60,
+        'pricePrint' => 8,
+        'priceShirt' => 11,
+        'specialPriceShirt' => 16
+    ];
