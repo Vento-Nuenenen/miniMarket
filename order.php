995a65cda577c003106b0793083b9c403ca6b69b
Caspar Brenneisen <github@brenneisen.me>
2022-11-14 18:50:37 +0100

    Update order.php

--- order.php
+++ order.php
@@ -1,6 +1,8 @@
 <?php
     include 'dbconnect.php';
 
+    $config = require 'config.php';
+
     $name = $_POST['name'];
     $contact = $_POST['contact'];
     $shirt_size = $_POST['shirt_size'];
@@ -9,12 +11,18 @@
     $print_placing = $_POST['print_placing'];
     $byos = $_POST['byos'] ? 1 : 0;
     $count = $_POST['count'];
+    if ($shirt_color == "tie_dye_black" || $shirt_color == "tie_dye_blue_green" || $shirt_color == "tie_dye"){
+        $price = ($config['specialPriceShirt'] + $config['pricePrint']) * $count;
+    } else {
+        $price = ($config['priceShirt'] + $config['pricePrint']) * $count;
+    }
+
 
     $query = "INSERT INTO `minimarket`
         (`name`, `contact`, `shirt_size`, `shirt_color`, 
-         `print_color`, `print_placing`, `byos`, `count`) 
+         `print_color`, `print_placing`, `byos`, `count`, `price`) 
         VALUES ('$name','$contact','$shirt_size','$shirt_color',
-                '$print_color','$print_placing','$byos','$count');";
+                '$print_color','$print_placing','$byos','$count', '$price');";
 
     $bestellung = mysqli_query($db, $query);
 
