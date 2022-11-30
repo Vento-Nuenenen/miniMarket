6f89134a07f0f7d7fbf491b7759da4afa682f370
Vento <github@brenneisen.me>
2022-11-30 21:06:41 +0100

    Update config.dist.php

--- config.dist.php
+++ config.dist.php
@@ -5,8 +5,5 @@
         'dbuser' => "root",
         'dbpass' => "1234",
         'dbname' => "minimarket",
-        'totalPriceImport' => 60,
         'pricePrint' => 8,
-        'priceShirt' => 11,
-        'specialPriceShirt' => 16
     ];
