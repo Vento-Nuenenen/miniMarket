35310b49f0ae732403f8522ec1f9ed4f92c6767f
Vento <github@brenneisen.me>
2023-01-15 15:31:59 +0100

    Create order.php

---
+++ named/order.php
@@ -0,0 +1,23 @@
+<?php
+    include 'dbconnect.php';
+
+    $config = require 'config.php';
+
+    $name = $_POST['name'];
+    $contact = $_POST['contact'];
+    $piece = $_POST['piece'];
+    $print_color = $_POST['print_color'];
+    $print_placing = $_POST['print_placing'];
+    $count = $_POST['count'];
+    $price = $config['pricePrint'];
+
+    $query = "INSERT INTO `minimarket`
+        (`name`, `contact`, `piece`, `print_color`, 
+         `print_placing`, `count`, `price`) 
+        VALUES ('$name','$contact', '$piece', '$print_color',
+                '$print_placing','$count', '$price');";
+
+    $bestellung = mysqli_query($db, $query);
+
+    header("Location: /success.php");
+    die();
