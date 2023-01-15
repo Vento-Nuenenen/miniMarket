4912366d56964a6f9f8cad5331e018ee246c6b07
Vento <github@brenneisen.me>
2023-01-15 15:37:29 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<header>
+    <title>miniMarket - Übersicht</title>
+</header>
+<body>
+    <h1>miniMarket</h1>
+
+    <p>
+        Folgende Bestellformulare bestehen aktuell:
+    </p>
+
+    <p>
+        Lasse deine Pfadi-Kleidung mit deinem Namen beschriften:
+        <a href="./named/index.php">Klicke hier</a>
+    </p>
+    <p>
+        Bestelle hier ein PfadiPink T-Shirt:
+        <a href="./pfadi-pink/index.php">Klicke hier</a>
+    </p>
+</body>
+</html>
