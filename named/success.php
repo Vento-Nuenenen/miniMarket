8e9db586351dfc61df5b8039eb81b003b0040b82
Vento <github@brenneisen.me>
2023-01-15 15:31:55 +0100

    Create success.php

---
+++ named/success.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>T-Shirt Bestellen</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+    <div id="app">
+        <main class="py-4">
+            <br />
+
+            <div class="container-fluid text-center">
+                <h1>Danke für deinen Auftrag.</h1>
+            </div>
+
+            <br />
+            <div class="container-fluid text-center">
+
+            </div>
+            <br />
+
+            <div class="container-fluid text-center">
+              <p>
+                  Danke für deinen Auftrag. <br />
+                  Sobald genügend Interessenten gefunden sind, werden die Teile verarbeitet. <br />
+                  Wir werden gelegentliche Updates für den Druck über den angegebenen Kontakt übermitteln. <br />
+                  <br />
+                  Falls du Fragen / Updates hast, melde dich unter: <a href="mailto:vento@pfadi-nuenenen.ch">vento@pfadi-nuenenen.ch</a> <br />
+              </p>
+            </div>
+        </main>
+
+        <script src="https://kit.fontawesome.com/b7304ac133.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    </div>
+</body>
