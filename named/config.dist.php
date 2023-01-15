bc071e8d928a1a1a006eed545667eb976365efeb
Vento <github@brenneisen.me>
2023-01-15 15:31:46 +0100

    Create config.dist.php

---
+++ named/config.dist.php
@@ -0,0 +1,9 @@
+<?php
+
+    return [
+        'dbhost' => "localhost",
+        'dbuser' => "root",
+        'dbpass' => "1234",
+        'dbname' => "minimarket",
+        'pricePrint' => 8,
+    ];
